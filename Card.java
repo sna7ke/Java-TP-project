b818a14f9feabcc4023bd8ed7230d4c5ef5fef78
Mohgithup <mohamedhamm284@gmail.com>
2024-12-19 20:11:45 +0100

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,4 +1,36 @@
 
 public class Card {
+package examtp;
+
+public class Card {
+    public enum Colour {
+        rouge("rouge"), blue("blue"), vert("vert"), jaune("jaune");
+
+        final private String nom;
+
+        private Colour(String nom) {
+            this.nom = nom;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+    }
+
+    private Colour couleur;
+
+    public Card(Colour couleur) {
+        this.couleur = couleur;
+    }
+
+    public Colour getCouleur() {
+        return couleur;
+    }
+
+    @Override
+    public String toString() {
+        return "Card: " + (couleur != null ? couleur.getNom() : "wild");
+    }
+}
 
 }
