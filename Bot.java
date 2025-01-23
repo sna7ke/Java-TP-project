978be6d620549fee62e7d470822b58e62ec9a037
Mohgithup <mohamedhamm284@gmail.com>
2025-01-23 21:30:12 +0100

    Update Bot.java

--- Bot.java
+++ Bot.java
@@ -12,10 +12,11 @@ public class Bot extends Player {
         ArrayList<Card> hand = getplayerHand();
         Card chosenCard = null;
 
+        // Recherche d'une carte jouable
         for (Card card : hand) {
             if (card.IsCardPlayable(topCard)) {
                 chosenCard = card;
-                hand.remove(card);
+                hand.remove(card); // Retirer la carte de la main du bot après l'avoir choisie
                 break;
             }
         }
@@ -24,13 +25,21 @@ public class Bot extends Player {
             System.out.println(getplayerName() + " played: " + chosenCard);
             chosenCard.ApplyEffect(game, new Scanner(System.in));
 
-            if (chosenCard instanceof WildCard) {
+            // Si c'est une carte spéciale (Wild Card ou Wild Draw Four)
+            if (chosenCard instanceof WildCard || chosenCard instanceof WildDrawFourCard) {
                 Card.Colour chosenColour = chooseRandomColour();
                 chosenCard.SetColour(chosenColour);
                 System.out.println(getplayerName() + " chose the color: " + chosenColour);
+
+                // Si le bot a joué un Wild Draw Four, le joueur suivant doit tirer 4 cartes
+                if (chosenCard instanceof WildDrawFourCard) {
+                    game.DrawCards(4);
+                }
             }
+
         } else {
             System.out.println(getplayerName() + " has no playable card and draws one.");
+            game.ProcessInput(0, this); // Tire une carte du paquet
         }
 
         return chosenCard;
